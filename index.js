235a276a9eaebbe18b0cb0f5a0a885fda6a6fc35
Elijahtum <kiprutoelijah@gmail.com>
2021-11-22 16:47:48 +0300

    Update to log in Details

--- index.js
+++ index.js
@@ -3,38 +3,89 @@
 let welcome = `Hi!\nwelcome to BABA TOSH SHOP`;
 console.log(welcome);
 
+console.log(`please select Account to log-In!!
+select 1 to login as admin 
+select 2 to login as customer`)
+
+
 //Login Options
-function login(admin, customer){
-    let admin= "babaTosh";
-    let customer = "Customer";
 
-}
 
-//User input 
+// Method 
+// User input prompt Method
 const prompt = require('prompt');
 
-const properties = [
-    {
-        name: 'username',
-        validator: /^[a-zA-Z\s\-]+$/,
-        warning: 'Username must be only letters, spaces, or dashes'
+const options = {
+    properties: {
+      option: {
+        description: "Option",
+        name: "number",
+        type: "number",
+        message: "Option must be a valid number",
+        required: true,
+      },
     },
-    {
-        name: 'password',
-        hidden: true
-    }
-];
+  };
+
+  const admin = {
+    properties: {
+      userName: {
+        validator: /^[a-zA-Z\s\-]+$/,
+        warning: 'Username must be only letters, spaces, or dashes',
+        required: true,
+      },
+      password: {
+          hiden: true,
+          replace: '*',
+      }
+    },
+  };
+
+  const newCustomer = {
+    properties: {
+      userName: {
+        validator: /^[a-zA-Z\s\-]+$/,
+        warning: 'Username must be only letters, spaces, or dashes',
+        required: true,
+      },
+      password: {
+          hiden: true,
+          replace: '*',
+      }
+    },
+  };
 
 prompt.start();
 
-prompt.get(properties, function (err, result) {
+prompt.get(options, function (err, result) {
     if (err) { return onErr(err); }
-    // console.log('Command-line input received:');
-    console.log('login Successfull! \nWelcome ' + result.username + ' To Baba Tosh Shop.');
-    // console.log('  Password: ' + result.password);
+    console.log(result.option)
+    if (result.option === 1) {
+        console.log(`Please enter your Admin Details to Log-In`)
+        prompt.get(admin, function (err, result){
+        console.log(`Hello, ${result.userName} what would you like to Do?
+        1) Stock Taking
+        2) Accounting 
+        3) Customer management`)
+        })
+    }
+    if (result.option === 2) {
+        console.log(`Please select if you are a new customer or returning 
+        1) New cutomer 
+        2) Returning Customer`)
+        prompt.get(options, function (err, result){
+            if (result.option === 1) {
+                console.log(`We would love to set you up in the system
+                please enter your details`)
+            }
+            prompt.get(admin)
+        })
+    }
 });
 
 function onErr(err) {
     console.log(err);
     return 1;
 }
+
+
